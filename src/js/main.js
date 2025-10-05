754824b6327b81ac15778ba12e9c0425522efc1e
indygriff79 <144857050+indygriff79@users.noreply.github.com>
2025-10-04 19:25:24 -0700

    fixed coding

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
-import { getParkData } from "./parkService.mjs";
+import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 
-const parkData = getParkData();
+
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+   introEl.innerHTML = `<h1>${parkData.fullName}</h1>
+  <p>${parkData.description}</p>`;
+
+
+
+}
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector(".info");
+  const html = data.map(mediaCardTemplate);
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+}
+
+async function init() {
+  const parkData = await getParkData();
+  const links = getinfoLinks(parkData.images);
+  setHeaderFooter();
+  setParkIntro(parkData);
+  setParkInfoLinks(links);
+}
+init();
+
--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -178,7 +178,52 @@ const park = {
   name: "Yellowstone",
   designation: "National Park"
 };
+export const parkInfoLinks = [
+{
+  name: "Current Conditions &#x203A;",
+  link: "conditions.html",
+  image: park.images[2].url,
+  description:
+  "See what conditions to expect in the park before leaving on your trip!"
+},
+{
+  name: "Fees and Passes &#x230A;",
+  link: "fees.html",
+  image: park.images[3].url,
+  description: "Learn about the fees and passes that are available."
+},
+{
+  name: "Visitor Centers &#x203A;",
+  link: "visitor_centers.html",
+  image: park.images[9].url,
+  description: "Learn about the vistor centers in the park."
+}
+];
+const baseUrl = "https://developer.nps.gov/api/v1/";
+const apiKey = import.meta.env.VITE_NPS_API_KEY;
 
-export function getParkData() {
-  return park;
+async function getJson(url) {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-Api-Key": apiKey
+  }
+  };
+  let data ={};
+  const response = await fetch(baseUrl + url, options);
+  if (response.ok) {
+    data = await response.json();
+  } else throw new Error("response not ok");
+  return data;
+}
+export function getinfoLinks(data) {
+  const withUpdatedImages = parkInfoLinks.map((item, index) => {
+    item.image = data[index + 2].url;
+    return item;
+  });
+  return withUpdatedImages;
+}
+export async function getParkData() {
+  const parkData = await getJson("parks?parkCode=yell");
+  return parkData.data[0];
 }
