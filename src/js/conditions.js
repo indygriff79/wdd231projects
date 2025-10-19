0b1ba547251d9d820d5ace1728ed66dc1a6eb633
indygriff79 <144857050+indygriff79@users.noreply.github.com>
2025-10-18 20:43:38 -0700

    moved conditions

---
+++ src/js/conditions.js
@@ -0,0 +1,41 @@
+import "../css/style.css";
+import "../css/conditions.css";
+import{
+    getParkData,
+    getParkAlerts,
+    getParkVisitorCenters
+} from "./parkSevice.mjs";
+
+import {
+    activityListTemplate,
+    alertTemplate,
+    visitorCenterTemplate
+} from "./templates.mjs";
+import setHeaderFooter from "./setHeadrerFooter.mjs";
+
+function setAlerts(alerts) {
+    const alertsContainer = document.querySelector(".alerts > ul");
+    alertsContainer,innerHTML = "";
+    const html = alerts.map(alertsTemplate);
+    alertsContainer.insertAdjacentHTML("afterbegin", html.join(""));
+}
+function setVisitorCenters(centers) {
+    const centersContainer = document.querySelector(".visitor ul");
+    const html = centers.map(visitorCenterTemplate);
+    centersContainer.insertAdjacentHTML("afterbegin", html.join(""));
+}
+function setActivities(activities) {
+    const activitiesContainer = document.querySelector(".activities ul");
+    const html = activityListTemplate(activities);
+    activitiesContainer.insertAdjacentHTML("afterbegin", html);
+}
+async function init() {
+    const parkData = await getParkData();
+    const alerts =await getParkAlerts(parkData.parkCode);
+    const visitorCenters = await getParkVisitorCenters(parkData.parkCode);
+    setHeaderFooter(parkData);
+    setAlerts(alerts);
+    setVisitorCenters(visitorCenters);
+    setActivities(parkData.activities);
+}
+init();
